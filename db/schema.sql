6428ec52335b9531d17f4896d1cc5e84f74c40d6
hausu <kennedymj2@gmail.com>
2019-07-14 20:20:25 -0500

    created db, wrote files for config, models, handlebars and server

--- db/schema.sql
+++ db/schema.sql
@@ -5,3 +5,14 @@
 --id auto increment /primary key
 --burger name 
 --devoured boolean
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,7 @@
 --querry to populate burgers table (at least 3)
+
+INSERT INTO burgers (burger, devoured) VALUES ('Impossible', false);
+INSERT INTO burgers (burger, devoured) VALUES ('Beyond', false);
+INSERT INTO burgers (burger, devoured) VALUES ('Morning Star', false);
+
+
