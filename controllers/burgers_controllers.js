adc4a51ffc79a2e9f0ebec964d35bc83150ca08c
hausu <kennedymj2@gmail.com>
2019-07-15 19:25:49 -0500

    fixed handlebar partials

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -10,11 +10,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -25,7 +25,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "burger", "deboured"
+    "burger", "devoured"
   ], [
     req.body.burger, req.body.devoured
   ], function(result) {
@@ -39,7 +39,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
@@ -54,7 +54,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
